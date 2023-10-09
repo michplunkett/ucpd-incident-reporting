eb37abcea39666b33e8ee3bb09bc87052fac7c1a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-09 17:19:24 -0500

    Update table.py

--- incident_reporting/pages/table.py
+++ incident_reporting/pages/table.py
@@ -1,2 +1,8 @@
+import streamlit as st
+
+from incident_reporting.external.google_nbd import GoogleNBD
+
+
 def get_spreadsheet():
-    pass
+    df = GoogleNBD().get_last_year_of_incidents()
+    st.dataframe(df.to_pandas(), hide_index=True)
