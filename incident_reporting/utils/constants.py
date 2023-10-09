4d66b5191c4794e68e1a583a45777f4d311974a5
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-09 16:06:11 -0500

    Update constants.py

--- incident_reporting/utils/constants.py
+++ incident_reporting/utils/constants.py
@@ -2,9 +2,11 @@ import os
 
 
 # Date/Time Constants
+TIMEZONE_CHICAGO = "America/Chicago"
 UCPD_DATE_FORMAT = "%m/%d/%y %I:%M %p"
 UCPD_MDY_DATE_FORMAT = "%m/%d/%Y"
 UCPD_MDY_KEY_DATE_FORMAT = "%Y-%m-%d"
+UTC_DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 # Environment Constants
 ENV_GCP_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "")
@@ -17,4 +19,6 @@ FILE_TYPE_JSON = "json"
 
 # Incident Constants
 INCIDENT_KEY_TYPE = "incident"
+INCIDENT_KEY_REPORTED = "reported"
 INCIDENT_KEY_REPORTED_DATE = "reported_date"
+INCIDENT_KEY_VALIDATED_LOCATION = "validated_location"
