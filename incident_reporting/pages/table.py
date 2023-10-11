69839af8fdba17e81a0f30da6f1f63ca8d3d43e0
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-11 00:15:22 -0500

    Update table.py

--- incident_reporting/pages/table.py
+++ incident_reporting/pages/table.py
@@ -1,8 +1,40 @@
 import streamlit as st
 
 from incident_reporting.external.google_nbd import GoogleNBD
+from incident_reporting.utils.constants import (
+    INCIDENT_KEY_COMMENTS,
+    INCIDENT_KEY_DISPOSITION,
+    INCIDENT_KEY_ID,
+    INCIDENT_KEY_REPORTED_DATE,
+    INCIDENT_KEY_TYPE,
+    INCIDENT_KEY_VALIDATED_ADDRESS,
+)
 
 
 def get_spreadsheet():
-    df = GoogleNBD().get_last_year_of_incidents()
-    st.dataframe(df.to_pandas(), hide_index=True)
+    df, incident_types = GoogleNBD().get_last_year_of_incidents()
+    print(df)
+    st.dataframe(
+        df.to_pandas(),
+        use_container_width=True,
+        column_order=[
+            INCIDENT_KEY_ID,
+            INCIDENT_KEY_TYPE,
+            INCIDENT_KEY_REPORTED_DATE,
+            INCIDENT_KEY_COMMENTS,
+            INCIDENT_KEY_VALIDATED_ADDRESS,
+        ],
+        column_config={
+            INCIDENT_KEY_DISPOSITION: "Disposition",
+            INCIDENT_KEY_COMMENTS: "Description",
+            INCIDENT_KEY_TYPE: "Incident Category",
+            INCIDENT_KEY_VALIDATED_ADDRESS: st.column_config.TextColumn(
+                "Validated Address"
+            ),
+            INCIDENT_KEY_ID: "UCPD ID",
+            INCIDENT_KEY_REPORTED_DATE: st.column_config.DateColumn(
+                "Reported Date", format="YYYY-MM-DD"
+            ),
+        },
+        hide_index=True,
+    )
