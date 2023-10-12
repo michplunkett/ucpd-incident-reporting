8621be7e059b110c552a0b1cc373f5ba3d40e657
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-11 23:22:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 """Serves as the entry point for the project module."""
-import streamlit as st
-
-from incident_reporting.pages.map import get_map
-from incident_reporting.pages.table import get_spreadsheet
-from incident_reporting.pages.time_histogram import get_time_histogram
+import dash
 
 
-def introduction():
-    st.write("# UCPD Incident Tracking")
-    st.markdown(
-        """
-        This project was undertaken with the goal of showing a more granular
+dash.register_page(__name__, path="/")
+
+dash.dcc.Markdown(
+    """
+    # UCPD Incident Tracking
+
+    This project was undertaken with the goal of showing a more granular
         picture of incidents are posted on the
         [UCPD Incident Report](https://incidentreports.uchicago.edu/) page.
 
@@ -24,15 +22,4 @@ def introduction():
         author, please reach out to me over email at
         **michplunkett[at]gmail[dot]com**.
     """
-    )
-
-
-page_names_to_funcs = {
-    "—": introduction,
-    "Map of the Last 90 Days": get_map,
-    "Spreadsheet of the last 365 Days": get_spreadsheet,
-    "Histogram of Incidents by Season": get_time_histogram,
-}
-
-demo_name = st.sidebar.selectbox("Choose a Visual", page_names_to_funcs.keys())
-page_names_to_funcs[demo_name]()
+)
