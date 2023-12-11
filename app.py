701573bf3b79b7634d85d7d2bd173b9b8fbd99fe
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-11 00:46:00 -0600

    Lots of work

--- app.py
+++ app.py
@@ -2,7 +2,12 @@
 from dash import Dash
 
 
-app = Dash(__name__, pages_folder="incident_reporting/pages/", use_pages=True)
+app = Dash(
+    __name__,
+    assets_folder="incident_reporting/assets/",
+    pages_folder="incident_reporting/pages/",
+    use_pages=True,
+)
 
 
 if __name__ == "__main__":
