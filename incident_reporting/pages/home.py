e6d12dd9477f6539d8e75cdd9ece33bf58adcb19
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-10 22:09:30 -0600

    Update home.py

--- incident_reporting/pages/home.py
+++ incident_reporting/pages/home.py
@@ -1,24 +1,24 @@
-import dash
+from dash import dcc, register_page
 
 
-dash.register_page(__name__, path="/")
+register_page(__name__, path="/")
 
-layout = dash.dcc.Markdown(
-    """
+layout = dcc.Markdown(
+    r"""
     # UCPD Incident Tracking
 
     This project was undertaken with the goal of showing a more granular
         picture of incidents are posted on the
         [UCPD Incident Report](https://incidentreports.uchicago.edu/) page.
 
-        The code for this project is stored in two places:
-        - The UCPD incident web-scraper:
-            [Link](https://github.com/michplunkett/ucpd-incident-scraper)
-        - The Starlit application for data display:
-            [Link](https://github.com/michplunkett/ucpd-incident-reporting)
+    The code for this project is stored in two places:
+    - The UCPD incident web-scraper:
+        [Link](https://github.com/michplunkett/ucpd-incident-scraper)
+    - The Starlit application for data display:
+        [Link](https://github.com/michplunkett/ucpd-incident-reporting)
 
-        For more information on this project, or any inquiries related to the
-        author, please reach out to me over email at
-        **michplunkett[at]gmail[dot]com**.
+    For more information on this project, or any inquiries related to the
+    author, please reach out to me over email at
+    **michplunkett\[at\]gmail\[dot\]com**.
     """
 )
