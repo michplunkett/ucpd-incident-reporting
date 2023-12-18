8428e28878f9fc29dcb73c1ddc9536fb21c0053f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-17 23:46:49 -0500

    Update hourly_summation.js

--- incident_reporting/static/js/hourly_summation.js
+++ incident_reporting/static/js/hourly_summation.js
@@ -1,1 +1,12 @@
 const hourlySummation = d3.select("#graph-container").append("svg");
+let seasonSummaries = {};
+
+async function getIncidents() {
+  const response = await fetch("/incidents/hourly");
+  return response.json();
+}
+
+getIncidents().then((r) => {
+  seasonSummaries = r["season_summaries"];
+  console.log(seasonSummaries);
+});
