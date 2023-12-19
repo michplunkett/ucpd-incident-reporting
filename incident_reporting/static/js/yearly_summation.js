e0745b1ec77f49fa45cd7f9894cd1dfc09356399
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-18 20:05:08 -0500

    Update yearly_summation.js

--- incident_reporting/static/js/yearly_summation.js
+++ incident_reporting/static/js/yearly_summation.js
@@ -1,1 +1,13 @@
+let typeCounts;
 const yearlySummation = d3.select("#visual-container").append("svg");
+
+async function getIncidents() {
+  const response = await fetch("/incidents/yearly");
+  return response.json();
+}
+
+getIncidents().then((r) => {
+  typeCounts = r["counts"];
+
+  console.log(typeCounts);
+});
