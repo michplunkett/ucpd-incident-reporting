51bbd98f465cd55f3a019c6ff6877fad521e41bb
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-01-14 19:04:55 -0600

    Update yearly_summation.js

--- incident_reporting/static/js/yearly_summation.js
+++ incident_reporting/static/js/yearly_summation.js
@@ -1,4 +1,4 @@
-let typeCounts;
+let counts, types;
 
 async function getIncidents() {
   const response = await fetch("/incidents/yearly");
@@ -6,5 +6,52 @@ async function getIncidents() {
 }
 
 getIncidents().then((r) => {
-  typeCounts = r["counts"];
+  types = r["types"];
+  counts = r["counts"];
+
+  Highcharts.chart("container", {
+    chart: {
+      type: "column",
+    },
+    title: {
+      text: "Frequency of UCPD Incident Types Over the Last Year",
+      align: "center",
+    },
+    subtitle: {
+      text: "This Graphic Pulls From the Last 365 Days",
+      align: "center",
+    },
+    xAxis: {
+      categories: types,
+      crosshair: true,
+      accessibility: {
+        description: "Countries",
+      },
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "1000 metric tons (MT)",
+      },
+    },
+    tooltip: {
+      valueSuffix: " (1000 MT)",
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+      },
+    },
+    series: [
+      {
+        name: "Corn",
+        data: [406292, 260000, 107000, 68300, 27500, 14500],
+      },
+      {
+        name: "Wheat",
+        data: [51086, 136000, 5500, 141000, 107180, 77000],
+      },
+    ],
+  });
 });
