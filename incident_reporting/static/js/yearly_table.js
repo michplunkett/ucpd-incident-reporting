9d94ebfa0f3bdb223c517414ca53818501e15f05
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-06-18 16:02:10 -0500

    Update yearly_table.js

--- incident_reporting/static/js/yearly_table.js
+++ incident_reporting/static/js/yearly_table.js
@@ -5,18 +5,19 @@ async function getIncidents() {
 
 function createTable() {
   getIncidents().then((r) => {
-    const incidents = r["incidents"];
+    const incidents = JSON.parse(r["incidents"]);
     const types = r["types"];
-    console.log(incidents);
+    console.log(incidents[0]);
     console.log(types);
-    const table = new DataTable("#visual-container", {
+    const table = new DataTable("#previous-year-incidents", {
       columns: [
-        { title: "UCPD ID" },
-        { title: "Category" },
-        { title: "Reported Time" },
-        { title: "Address" },
-        { title: "Description" },
+        { title: "UCPD ID", data: "ucpd_id" },
+        { title: "Category", data: "incident" },
+        { title: "Reported Time", data: "reported" },
+        { title: "Location", data: "location" },
+        { title: "Description", data: "comments" },
       ],
+      data: incidents,
       fixedHeader: true,
       paging: true,
       responsive: true,
