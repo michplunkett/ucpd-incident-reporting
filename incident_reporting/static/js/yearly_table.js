88f820ae82e0d1167721138e13c9495fa05bab57
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-06-18 15:44:59 -0500

    Update yearly_table.js

--- incident_reporting/static/js/yearly_table.js
+++ incident_reporting/static/js/yearly_table.js
@@ -7,5 +7,21 @@ function createTable() {
   getIncidents().then((r) => {
     const incidents = r["incidents"];
     const types = r["types"];
+    console.log(incidents);
+    console.log(types);
+    const table = new DataTable("#visual-container", {
+      columns: [
+        { title: "UCPD ID" },
+        { title: "Category" },
+        { title: "Reported Time" },
+        { title: "Address" },
+        { title: "Description" },
+      ],
+      fixedHeader: true,
+      paging: true,
+      responsive: true,
+    });
+
+    table.draw();
   });
 }
